d5f079850e9c343939a16c1d5d5bf6d6825214a0
Brien McCurdy <brien.mccurdy@gmail.com>
2019-08-05 12:39:51 -0400

    Create base.js (#5)
    PAN-040 Added base js file

---
+++ js/base.js
@@ -0,0 +1,3 @@
+// This file won't be ready by end of Sprint because its poorly written but let's try and jam it in anyway
+
+document.getElementByID('pan-title').setAttribute('class', "title-color_blue");
