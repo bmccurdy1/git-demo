787a4fac447023480c69e103b3316d6760fde75d
Brien McCurdy <brien.mccurdy@gmail.com>
2019-08-05 13:27:00 -0400

    Removed 040, not making it to sprint 10 (#7)

--- js/base.js
+++
@@ -1,3 +0,0 @@
-// This file won't be ready by end of Sprint because its poorly written but let's try and jam it in anyway
-
-document.getElementByID('pan-title').setAttribute('class', "title-color_blue");
